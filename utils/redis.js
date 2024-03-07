7be2a7bc9b2c507f6c203d644b6268c0032c8828
H. Sabah <sabahhasbi00@gmail.com>
2024-03-07 22:12:41 +0000

    Files added: task 0. Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+const redis = require("redis");
+import { promisify } from "util";
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on("error", (err) => console.log(err));
+    this.Get = promisify(this.client.get).bind(this.client);
+    this.Del = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    const res = this.client.on("ready", () => true);
+    return res ? true : false;
+  }
+
+  async get(key) {
+    return this.Get(key).then((value) => value);
+  }
+
+  async set(key, value, duration) {
+    const SetExp = promisify(this.client.set).bind(this.client);
+    await SetExp(key, value);
+    await this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    this.Del(key);
+  }
+}
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
+// export default RedisClient
