ab5232b619d04a790749b141c7819ddaf3cc3082
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-03-09 16:01:57 +0200

    change to the isAlive

--- utils/redis.js
+++ utils/redis.js
@@ -2,32 +2,37 @@ import { promisify } from 'util';
 import { createClient } from 'redis';
 
 class RedisClient {
-  constructor() {
+ constructor() {
     this.client = createClient();
     this.client.on('error', (err) => console.log(err));
+    this.client.on('ready', () => {
+      this.isConnected = true;
+    });
+    this.Get = promisify(this.client.get).bind(this.client);
+    this.SetExp = promisify(this.client.set).bind(this.client);
     this.Del = promisify(this.client.del).bind(this.client);
   }
   
   isAlive() {
-    const res = this.client.on('ready', () => true);
-    return res;
+    return this.isConnected;
   }
   
   async get(key) {
-    this.Get = promisify(this.client.get).bind(this.client);
     return this.Get(key).then((value) => value);
   }
 
   async set(key, value, duration) {
-    const SetExp = promisify(this.client.set).bind(this.client);
-    await SetExp(key, value);
-    await this.client.expire(key, duration);
+    await this.SetExp(key, value);
+    if (duration) {
+      await this.client.expire(key, duration);
+    }
   }
 
   async del(key) {
     await this.Del(key);
   }
 }
+
 const redisClient = new RedisClient();
 
 module.exports = redisClient;
